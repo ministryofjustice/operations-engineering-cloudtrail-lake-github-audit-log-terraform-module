3d8831261388b0aee4d038e8ac20be6e8bee906b
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-04-10 17:16:36 +0100

    remove unecessary stuff 2

--- terraform/terraform-aws-cloudtrail-lake-github-audit-log/examples/new_github_audit_log_s3_bucket/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "github_auditlog_s3bucket" {
-  value = module.github-cloudtrail-auditlog.github_auditlog_s3bucket
-}
-
-output "github_s3_reader_function" {
-  value = module.github-cloudtrail-auditlog.github_s3_reader_function
-}
